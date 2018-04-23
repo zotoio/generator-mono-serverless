1f681c184337af6be2d4295cba9e04a1e56e4813
wyvern8 <wyvern@smartcreations.com.au>
2018-04-23 22:07:33 +1000

    fix(env): var naming

--- generators/package/templates/src/index.ts
+++ generators/package/templates/src/index.ts
@@ -9,12 +9,14 @@ interface HelloResponse {
 }
 
 const handler: Handler = (event: any, context: Context, callback: Callback) => {
-    AWS.config.update({ region: process.env.GTM_AWS_REGION });
+    AWS.config.update({ region: process.env.SLS_AWS_REGION });
 
     const response: HelloResponse = {
         statusCode: 200,
         body: JSON.stringify(event)
     };
+
+    console.log('use console log, and view cloudwatch logs');
 
     callback(undefined, response);
 };
