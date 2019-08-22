c5d6a6bf922177035e46d1bd987c3e396cc2146f
Decebal Dobrica <decebal.dobrica@tellimer.com>
2019-08-22 10:58:19 +0100

    replace jest with mocha, add semantic versioning

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,9 +1,16 @@
 // webpack.config.js
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-    entry: slsw.lib.entries,
-    target: 'node',
-    devtool: 'source-map',
-    mode: process.env.NODE_ENV || 'production'
+  entry: slsw.lib.entries,
+  target: 'node',
+  devtool: 'source-map',
+  mode: process.env.NODE_ENV || 'production',
+  externals: [
+    nodeExternals(),
+    nodeExternals({
+      modulesFromFile: true,
+    }),
+  ],
 };
